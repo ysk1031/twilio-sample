460f991a9f01a35eb763662fc1f7c7a004171eb3
Yusuke AONO <yusukeaono.1031@gmail.com>
2013-08-05 21:34:45 +0900

    git rm constant.rb.

--- config/initializers/constant.rb
+++
@@ -1,5 +0,0 @@
-APP_URL = "http://salty-springs-9472.herokuapp.com"
-
-TWILIO_PHONE_NUMBER = "+815031318955"
-TWILIO_SID = "ACc9884e1c1aaf4af739fc126b0ee978bd"
-TWILIO_TOKEN = "5555df0bca5c7eb8f6a0fc7f90bed0a3"
